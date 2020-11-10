2f503181acc19d4925ee3899bbfdf00b093e1a36
Dao Duc Thao <daoducthao99tb@gmail.com>
2020-11-10 21:50:29 +0700

    first commit

---
+++ OnlineShop/OnlineShop/Areas/Admin/Controllers/ContentController.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Model.Dao;
+using Model.EF;
+using OnlineShop.Common;
+
+namespace OnlineShop.Areas.Admin.Controllers
+{
+    public class ContentController : BaseController
+    {
+        // GET: Admin/Content
+        public ActionResult Index(string searchString, int page = 1, int pageSize = 10)
+        {
+            var dao = new ContentDao();
+            var model = dao.ListAllPaging(searchString, page, pageSize);
+
+            ViewBag.SearchString = searchString;
+            return View(model);
+        }
+        [HttpGet]
+        public ActionResult Create()
+        {
+            SetViewBag();
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Edit(long id)
+        {
+            var dao = new ContentDao();
+            var content = dao.GetByID(id);
+            SetViewBag(content.ID);
+            return View(content);
+        }
+
+        [HttpPost]
+        [ValidateInput(false)]
+        public ActionResult Create(Content model)
+        {
+            if (ModelState.IsValid)
+            {
+                var session = (UserLogin)Session[CommonConstants.USER_SESSION];
+                model.CreateBy = session.UserName;
+                var culture = Session[CommonConstants.CurrentCulture];
+                model.Language = culture.ToString();
+                new ContentDao().Create(model);
+                return RedirectToAction("Index");
+            }
+            SetViewBag();
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateInput(false)]
+        public ActionResult Edit(Content model)
+        {
+            if (ModelState.IsValid)
+            {
+                var session = (UserLogin)Session[CommonConstants.USER_SESSION];
+                model.CreateBy = session.UserName;
+                var culture = Session[CommonConstants.CurrentCulture];
+                model.Language = culture.ToString();
+                new ContentDao().Edit(model);
+                return RedirectToAction("Index");
+            }
+            SetViewBag(model.CategoryID);
+            return View();
+        }
+        public void SetViewBag(long? selectedID = null)
+        {
+            var dao = new CategoryDao();
+            ViewBag.CategoryID = new SelectList(dao.ListAll(), "ID", "Name", selectedID);
+           
+        }
+    }
+}
---
+++ OnlineShop/OnlineShop/Areas/Admin/Controllers/ProductController.cs
@@ -0,0 +1,66 @@
+﻿using Model.Dao;
+using Model.EF;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OnlineShop.Areas.Admin.Controllers
+{
+    public class ProductController : BaseController
+    {
+        // GET: Admin/Product
+        public ActionResult Index(string searchString, int page = 1, int pageSize = 5)
+        {
+            var dao = new ProductDao();
+            var model = dao.ListAllPaging(searchString, page, pageSize);
+
+            ViewBag.SearchString = searchString;
+            return View(model);
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            SetViewBag();
+            return View();
+        }
+
+        //[HttpPost]
+        //public ActionResult Create(Product product)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        var dao = new ProductDao();
+
+                
+
+        //        long id = dao.Insert(product);
+        //        if (id > 0)
+        //        {
+        //            SetAlert("Thêm sản phẩm thành công", "success");
+        //            return RedirectToAction("Index", "Product");
+        //        }
+        //        else
+        //        {
+        //            ModelState.AddModelError("", "Thêm sản phẩm không thành công");
+        //        }
+        //    }
+        //    return View("Index");
+        //}
+
+        public void SetViewBag(long? selectedId = null)
+        {
+            var dao = new ProductCategoryDao();
+            ViewBag.CategoryID = new SelectList(dao.ListAll(), "ID", "Name", selectedId);
+        }
+        [HttpDelete]
+        public ActionResult Delete(int id)
+        {
+            new ProductDao().Delete(id);
+
+            return RedirectToAction("Index");
+        }
+    }
+}
